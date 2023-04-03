3826cbc4816381f201e22e873214847951ca1fd6
yosepvvictor <victoryosep221@gmail.com>
2023-04-03 20:07:57 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <div className='text-4xl'>ABC</div>
+    </div>
+  )
+}
+
+export default App
